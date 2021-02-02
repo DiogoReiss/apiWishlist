917c20fc525490daa95014878be3330cb7566146
DiogoReiss <diogommreis1@gmail.com>
2021-02-02 05:11:04 +0000

    addying port to .env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './routes'
+import routes from './routes';
+import dotenv from 'dotenv';
 import loggingMiddleware from './middlewares/logging';
 import { connectDB } from './services/databaseConnection';
 import log from './utils/logging';
@@ -8,12 +9,13 @@ import log from './utils/logging';
 const api = express();
 
 connectDB();
+dotenv.config();
 
 api.use(cors());
 api.use(express.json());
 api.use(loggingMiddleware);
 api.use(routes)
 
-api.listen(3333, () => {
+api.listen(process.env.PORT || 5000, () => {
   log.info("initAPI", "Server started on http://localhost:3333")
 });
